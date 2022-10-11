bcbe3cd37205388f18483ce6a5b57730af3288e2
jfnoy <jfnoy@keralty.com>
2022-10-11 17:19:54 -0500

    modificacion interfaz servicio

--- src/main/java/org/healthcare/springcloud/msvc/programas/services/IProgramaService.java
+++ src/main/java/org/healthcare/springcloud/msvc/programas/services/IProgramaService.java
@@ -10,15 +10,6 @@ public interface IProgramaService {
 	List<Programa> findAll();
     Optional<Programa> findById(Long id);    
     Programa save(Programa programa);
-    void deleteById(Long id);
-    
-    //void eliminarCursoUsuarioPorId(Long id);
-
-    /*
-    metodos remotos
-     */
-    //Optional<Paciente> asignarUsuario(Paciente paciente, Long programaId);
-    //Optional<Paciente> crearUsuario(Paciente paciente, Long programaId);
-    //Optional<Paciente> eliminarUsuario(Paciente paciente, Long programaId);
+    void deleteById(Long id);      
 
 }
